c4f4a598f0330f783220a340bc050324a074ea06
sana-hm <sana.sh19@yahoo.com>
2021-07-15 13:39:06 +0200

    add JavaEvaluation Files

--- JavaEvaluation/src/eval/java/Main.java
+++ JavaEvaluation/src/eval/java/Main.java
@@ -1,8 +1,24 @@
 package eval.java;
 
+import tools.java.TransCoder;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.StandardOpenOption;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 	// write your code here
+        String testGermain = "6Qe0IsEEH1utWRe7UKzGMiDTytOB3HS1dEfIB4imna3IRHXHRn5ZrvKFEcPjmPgKYGuytG+gDAl1m2DdHalJQg==";
+        TransCoder transcodeTest = new TransCoder(testGermain);
+
+        StringBuilder str = transcodeTest.decode("BYAPASBNBGAPASBGASBNASAFBHBGBNAHAJBNAZAFBLADBNAPASASAJAMAPADBNBJBJBJ");
+
+        System.out.println(str);
+
+
     }
 }
--- JavaEvaluation/src/tools/java/TransCoder.java
+++ JavaEvaluation/src/tools/java/TransCoder.java
@@ -1,20 +1,75 @@
 package tools.java;
 
+import org.apache.commons.lang3.StringUtils;
+import org.germain.tool.ManaBox;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class TransCoder {
 
-    public TransCoder(String testGermain) {
+    private HashMap<Character,String> encode = new HashMap<>();
+    private  HashMap<String,Character> decode = new HashMap<>();
 
+    public TransCoder(String str) {
+
+        String keys = ManaBox.decrypt(str);
+        StringBuilder values = new StringBuilder();
+
+        char ch1 ='A';
+        char ch2 = 'A';
+
+        for (int i = 0; i < keys.length() ; i++) {
+
+            values.append(ch1);
+            values.append(ch2);
+            ch2++;
+            if (ch2 > 'Z') {
+
+                ch1++;
+                ch2 = 'A';
+            }
+        }
+        for (int i = 0; i < keys.length()  ; i++) {
+
+            encode.put(keys.charAt(i), values.substring((i * 2), (i * 2 )+ 2 ));
+            decode.put( values.substring((i * 2), (i * 2 )+ 2 ),keys.charAt(i));
+        }
     }
 
-    public Object getDecode() {
-
-        return null;
+    public HashMap<Character, String> getEncode() {
+        return encode;
     }
 
+    public HashMap<String, Character> getDecode() {
+        return decode;
+    }
 
-    public Object getEncode() {
+    public StringBuilder encode (String str){
 
-        return null;
+        StringBuilder crypt = new StringBuilder();
+        str = StringUtils.stripAccents(str);
+
+        for (int i = 0; i < str.length() ; i++) {
+            crypt.append(encode.get(str.charAt(i)));
+        }
+        return crypt;
+    }
+
+    public StringBuilder decode (String strCode){
+
+        String[] strArr = new String[strCode.length() / 2];
+        StringBuilder decrypt = new StringBuilder();
+
+        for (int i = 0; i < strArr.length; i++) {
+            strArr[i] = strCode.substring((i * 2 ),(i * 2 ) +2);
+        }
+
+        for (String s:strArr) {
+            decrypt.append(decode.get(s));
+        }
+        return decrypt;
     }
 
 }
