c45d6afdb7a9ef9e2a7c3ccac381c73c1ec43f38
sana-hm <sana.sh19@yahoo.com>
2021-08-24 11:20:17 +0200

    finished exercise ListView Android

---
+++ Android/ListView/app/src/main/java/com/example/listview/UserAccount.java
@@ -0,0 +1,53 @@
+package com.example.listview;
+
+import androidx.annotation.NonNull;
+
+public class UserAccount {
+
+    private String userName;
+    private String userType;
+
+    private boolean active;
+
+    public UserAccount(String userName, String userType) {
+        this.userName = userName;
+        this.userType = userType;
+        this.active = true;
+    }
+
+    public UserAccount(String userName, String userType, boolean active) {
+        this.userName = userName;
+        this.userType = userType;
+        this.active = active;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return this.userName+" ("+this.userType+")";
+    }
+}
