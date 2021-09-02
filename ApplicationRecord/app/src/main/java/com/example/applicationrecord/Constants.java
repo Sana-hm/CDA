5fe23dc466d4a564332d767cb7d85b338ba69a63
sana-hm <sana.sh19@yahoo.com>
2021-09-02 16:42:39 +0200

    update project Application Android Record

---
+++ ApplicationRecord/app/src/main/java/com/example/applicationrecord/Constants.java
@@ -0,0 +1,14 @@
+package com.example.applicationrecord;
+
+public class Constants {
+
+    //  base url qui note api
+    private static final String ROOT_URL = "https://127.0.1.1:8000/model/api/";
+
+
+    // le nom du fichier utilisé
+    public static final String URL_CREATE = ROOT_URL + "create.php";
+    public static final String URL_READ = ROOT_URL + "read.php";
+    public static final String URL_UPDATE = ROOT_URL + "update.php";
+    public static final String URL_DELETE = ROOT_URL + "delete.php";
+}
--- ApplicationRecord/app/src/main/java/com/example/applicationrecord/MainActivity.java
+++ ApplicationRecord/app/src/main/java/com/example/applicationrecord/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.applicationrecord;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        listView = findViewById(R.id.listView);
+        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+
+        addDiscs();
+
+        // utiliser customListAdapter sur listView
+        listView.setAdapter(new DiscListAdapter(discs, this));
+
+    }
+
+    private void addDiscs() {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, Constants.URL_READ, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        System.out.println(response.toString());
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.print(error.toString());
+
+                    }
+                });
+
+        // Accédez à RequestQueue via mon classe singleton.
+        RequestHandler.getInstance(this).addToRequestQueue(jsonObjectRequest);
     }
 }
